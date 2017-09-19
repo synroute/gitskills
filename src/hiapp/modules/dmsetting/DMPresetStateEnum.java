b23d20275d50a8694c85d5941363b858f39a2a1f
yangwentian <yangwentian@YWTComputer>
2017-09-19 20:50:33 +0800

    提交

--- src/hiapp/modules/dmsetting/DMPresetStateEnum.java
+++ src/hiapp/modules/dmsetting/DMPresetStateEnum.java
@@ -1,26 +1,27 @@
 package hiapp.modules.dmsetting;
+
 /** 
  * @author yangwentian 
  * @version 创建时间：2017年9月19日 下午8:31:35 
  * 类说明 预约状态枚举
  */
 public enum DMPresetStateEnum {
-	InUse(1,"使用中"),
-	FinishPreset(2,"预约完成");
-	private DMPresetStateEnum(int id,String stateName) {
-		this.setId(id);
+	InUse("使用中"),
+	FinishPreset("预约完成");
+	private String	stateName;//状态名
+	
+	private DMPresetStateEnum(String stateName) {
 		this.setStateName(stateName);	
     }
 	
-	private int	id;//id
-	private String	stateName;//状态名
-	public int getId() {
-		return id;
+	public static DMPresetStateEnum getStateString(String name) {
+		if (DMPresetStateEnum.InUse.getStateName().equals(name))
+			return DMPresetStateEnum.InUse;
+		
+		if (DMPresetStateEnum.FinishPreset.getStateName().equals(name))
+			return DMPresetStateEnum.FinishPreset;
+		return null;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getStateName() {
 		return stateName;
 	}
@@ -28,4 +29,5 @@ public enum DMPresetStateEnum {
 	public void setStateName(String stateName) {
 		this.stateName = stateName;
 	}	
+	
 }
