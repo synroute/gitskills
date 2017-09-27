936622e843df01bd7b546b6dfa772e3f7d380091
cuilulu <cuilulu@cuilulu-PC>
2017-09-27 12:03:53 +0800

    崔路路

--- src/hiapp/modules/dmmanager/bean/ExcelUtils.java
+++ src/hiapp/modules/dmmanager/bean/ExcelUtils.java
@@ -1,18 +1,25 @@
 package hiapp.modules.dmmanager.bean;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import hiapp.utils.DbUtil;
+import hiapp.utils.database.BaseRepository;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.JOptionPane;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
@@ -22,10 +29,17 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
 
 
-public class ExcelUtils {
+public class ExcelUtils extends BaseRepository{
+	@SuppressWarnings("deprecation")
 	public  void exportExcel(List<String> excelHeader,List<Map<String,Object>> dataList,List<String> sheetCulomn,HttpServletRequest request,HttpServletResponse response){
 		 HSSFWorkbook workbook = new HSSFWorkbook();                        // 创建工作簿对象
          HSSFSheet sheet = workbook.createSheet();                     // 创建工作表
@@ -103,9 +117,8 @@ public class ExcelUtils {
 		try {
 			 String fileName = "Excel-" + String.valueOf(System.currentTimeMillis()).substring(4, 13) + ".xls";
 	         String headStr = "attachment; filename=\"" + fileName + "\"";
-	         //FileOutputStream out = new FileOutputStream("E://学生表.xls");
 	         OutputStream out = response.getOutputStream();
-	        response.setContentType("application/force-download");// 设置强制下载不打开
+	         response.setContentType("application/force-download");// 设置强制下载不打开
 	         response.setContentType("APPLICATION/OCTET-STREAM");
 	         response.setHeader("Content-Disposition", headStr);
 			 workbook.write(out);
@@ -118,11 +131,183 @@ public class ExcelUtils {
          
 	}
 	
+	@SuppressWarnings({"deprecation" })
+	public void excelImport(HttpServletRequest request,MultipartFile file,String tableName){
+		 List<String> excelHeaderList=new ArrayList<String>();//表头集合
+		 List<List<String>> excelDataList=new ArrayList<List<String>>();
+		 String fileName=file.getOriginalFilename();
+		try {
+			InputStream in = file.getInputStream();
+			Workbook workbook =null;
+			String suffix=fileName.substring(fileName.indexOf("."));//获取后缀名
+			if(".xls".equals(suffix)){ 
+				workbook = new HSSFWorkbook(in);
+			}else if(".xlsx".equals(suffix)){
+				workbook = new XSSFWorkbook(in);
+			}
+			Sheet sheet = workbook.getSheetAt(0);
+			Row firstRow=sheet.getRow(0);
+			int totalRowNum = sheet.getLastRowNum()+1; //总行数
+			int coloumNum=firstRow.getPhysicalNumberOfCells();//总列数
+			for (int i = 0; i < coloumNum; i++) {
+				String cellVlue=null;
+            	if(firstRow.getCell(i)!=null){
+            		String value=getStringcell(firstRow.getCell(i));
+            		if(value!=null){
+            			cellVlue=value;
+            		}else{
+            			firstRow.getCell(i).setCellType(Cell.CELL_TYPE_STRING);
+		            	cellVlue=firstRow.getCell(i).getRichStringCellValue().toString();
+            		}
+            			excelHeaderList.add(cellVlue);
+               }else{
+            	   excelHeaderList.add("");
+               }
+			}
+			
+			for(int i = 1; i < totalRowNum; i++){
+				 //获取当前行的数据
+	            Row row = sheet.getRow(i);
+	            List<String>  list=new ArrayList<String>();
+	            for (int j = 0; j < coloumNum; j++) {
+	            	String cellVlue=null;
+	            	if(row.getCell(j)!=null){
+	            		String value=getStringcell(row.getCell(j));
+	            		if(value!=null){
+	            			cellVlue=value;
+	            		}else{
+	            			row.getCell(j).setCellType(Cell.CELL_TYPE_STRING);
+			            	cellVlue=row.getCell(j).getRichStringCellValue().toString();
+	            		}
+	         	           list.add(cellVlue);
+	            	
+	               }else{
+	            	   list.add("");
+	               }
+				}
+	           excelDataList.add(list);
+			}
+			insertDataToDb(excelDataList,tableName,coloumNum);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		
+	}
+	
+	
+	public  void  insertDataToDb(List<List<String>> excelDataList,String tableName,Integer coloumNum){
+		Connection conn=null;
+		PreparedStatement pst = null;
+		Statement statement=null;
+		ResultSet rs=null;
+		String tempTableName=tableName+"_TEMP";
+		
+		try {
+			conn=this.getDbConnection();
+			String sql="select * from "+tableName+" where rownum=1";
+			pst=conn.prepareStatement(sql);
+			rs=pst.executeQuery();
+			ResultSetMetaData data=rs.getMetaData();
+			int columnCount=data.getColumnCount();
+			String creatTableSql="create table "+tempTableName+"(";
+			while(rs.next()){
+				for (int i = 1; i <= columnCount; i++) {
+					String columnName=data.getColumnName(i);
+					int columnType = data.getColumnType(i);
+					int columnSize=data.getColumnDisplaySize(i);
+					creatTableSql+=columnName;
+					if(Types.VARCHAR==columnType){
+						creatTableSql+="  varchar2("+columnSize+"),";
+					}else if(Types.INTEGER==columnType){
+						creatTableSql+=" number,";
+					}else if(Types.DATE==columnType){
+						creatTableSql+=" date,";
+					}
+					
+				}
+				creatTableSql=creatTableSql.substring(0,creatTableSql.length()-1)+")";
+				pst=conn.prepareStatement(creatTableSql);
+				pst.executeUpdate();
+				statement=conn.createStatement();
+				for (int i = 0; i<excelDataList.size(); i++) {
+					List<String> columnDataList=excelDataList.get(i);
+					String insertDataSql=" insert into "+tempTableName+"(";
+					String valueSql=" values(";
+					for (int j = 1; j  <= columnCount; j++) {
+						String columnName=data.getColumnName(j);
+						int columnType = data.getColumnType(j);
+						if(columnCount<=coloumNum){
+							insertDataSql+=columnName+",";
+							if(Types.VARCHAR==columnType){
+								valueSql+="'"+columnDataList.get(j-1)+"',";
+							}else if(Types.INTEGER==columnType){
+								valueSql+=columnDataList.get(j-1)+",";
+							}else if(Types.DATE==columnType){
+								valueSql+="to_date('"+columnDataList.get(j-1)+"','yyyy-mm-dd')"+",";
+							}
+						}else if(columnCount>coloumNum){
+							if(j<coloumNum){
+								insertDataSql+=columnName+",";
+								if(Types.VARCHAR==columnType){
+									valueSql+="'"+columnDataList.get(j-1)+"',";
+								}else if(Types.INTEGER==columnType){
+									valueSql+=columnDataList.get(j-1)+",";
+								}else if(Types.DATE==columnType){
+									valueSql+="to_date('"+columnDataList.get(j-1)+"','yyyy-mm-dd')"+",";
+								}
+							}
+						}
+					}
+					
+					insertDataSql=insertDataSql.substring(0,insertDataSql.length()-1)+")"+valueSql.substring(0,valueSql.length()-1)+")";
+					statement.addBatch(insertDataSql);
+				}
+				
+				statement.executeBatch();
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			DbUtil.DbCloseQuery(rs,pst);
+			try {
+				if(statement!=null){
+					statement.close();
+				}
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			DbUtil.DbCloseConnection(conn);
+		}
+	}
+	
+	public String getStringcell(Cell cell){
+		String value=null;
+		if("yyyy/mm/dd".equals(cell.getCellStyle().getDataFormatString()) || "m/d/yy".equals(cell.getCellStyle().getDataFormatString())
+		        || "yy/m/d".equals(cell.getCellStyle().getDataFormatString()) || "mm/dd/yy".equals(cell.getCellStyle().getDataFormatString())
+		        || "dd-mmm-yy".equals(cell.getCellStyle().getDataFormatString())|| "yyyy/m/d".equals(cell.getCellStyle().getDataFormatString())){
+			value= new SimpleDateFormat("yyyy/MM/dd").format(cell.getDateCellValue());
+		}
+		return value;
+	}
+	
+	
+	
+	
+	
 	
 	 /*  
      * 列数据信息单元格样式
      */  
-      public HSSFCellStyle getStyle(HSSFWorkbook workbook) {
+      @SuppressWarnings("deprecation")
+	public HSSFCellStyle getStyle(HSSFWorkbook workbook) {
             // 设置字体
             HSSFFont font = workbook.createFont();
             //设置字体大小
@@ -165,7 +350,8 @@ public class ExcelUtils {
       /* 
        * 列头单元格样式
        */    
-        public HSSFCellStyle getColumnTopStyle(HSSFWorkbook workbook) {
+        @SuppressWarnings("deprecation")
+		public HSSFCellStyle getColumnTopStyle(HSSFWorkbook workbook) {
             
               // 设置字体
             HSSFFont font = workbook.createFont();
