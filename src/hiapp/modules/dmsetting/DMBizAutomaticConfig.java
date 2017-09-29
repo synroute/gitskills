bfe331c208075e8cf88f1825625cfdf04355345c
synliuhao <synliuhao@synliuhao-PC>
2017-09-29 17:26:29 +0800

    dm修改自动生成

--- src/hiapp/modules/dmsetting/DMBizAutomaticConfig.java
+++ src/hiapp/modules/dmsetting/DMBizAutomaticConfig.java
@@ -11,6 +11,12 @@ public class DMBizAutomaticConfig {
 	private String pageName;
 	private String panleName;
 	private String config;
+	private String creater;
+	private String state;
+	private String displayType;
+	private String createTime;
+	private int pageId;
+	private int isDelete;
 	public String getId() {
 		return id;
 	}
@@ -46,5 +52,41 @@ public class DMBizAutomaticConfig {
 	}
 	public void setConfig(String config) {
 		this.config = config;
+	}
+	public String getCreater() {
+		return creater;
+	}
+	public void setCreater(String creater) {
+		this.creater = creater;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getDisplayType() {
+		return displayType;
+	}
+	public void setDisplayType(String displayType) {
+		this.displayType = displayType;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public int getPageId() {
+		return pageId;
+	}
+	public void setPageId(int pageId) {
+		this.pageId = pageId;
+	}
+	public int getIsDelete() {
+		return isDelete;
+	}
+	public void setIsDelete(int isDelete) {
+		this.isDelete = isDelete;
 	}
 }
--- src/hiapp/modules/dmsetting/srv/AutomaticController.java
+++ src/hiapp/modules/dmsetting/srv/AutomaticController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +21,9 @@ import hiapp.modules.dmsetting.result.*;
 import hiapp.modules.dmsetting.DMBizAutomaticConfig;
 import hiapp.modules.dmsetting.DMBizImportTemplate;
 import hiapp.modules.dmsetting.data.DmBizAutomaticRepository;
+import hiapp.system.buinfo.User;
 import hiapp.system.worksheet.bean.WorkSheet;
+import hiapp.utils.idfactory.IdFactory;
 import hiapp.utils.serviceresult.RecordsetResult;
 import hiapp.utils.serviceresult.ServiceResult;
 import hiapp.utils.serviceresult.ServiceResultCode;
@@ -29,7 +33,8 @@ public class AutomaticController {
 	private DmBizAutomaticRepository dmBizAutomatic;
 	@Autowired
 	private WorkSheet worksheet;
-	
+	@Autowired
+	private IdFactory idFactory;
 	@RequestMapping(value = "/srv/dm/dmGetBizResultColumns.srv", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	public String dmGetBizResultColumns(@RequestParam("bizId") String bizId) {
 		RecordsetResult recordsetResult = new RecordsetResult();
@@ -130,10 +135,10 @@ public class AutomaticController {
 	
 	@RequestMapping(value = "srv/dm/dmCreateAutomaticPageUrl.srv", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	public String dmCreateAutomaticPageUrl(@RequestParam("bizId") int bizId,@RequestParam("pageName") String pageName,
-			@RequestParam("pageUrl") String pageUrl) {
+			@RequestParam("pageUrl") String pageUrl,@RequestParam("pageParameter") String pageParameter) {
 		
 		ServiceResult serviceresult = new ServiceResult();
-		if(dmBizAutomatic.dmCreateAutomaticPageUrl(bizId,pageName,pageUrl))
+		if(dmBizAutomatic.dmCreateAutomaticPageUrl(bizId,pageName,pageUrl,pageParameter))
 		{
 			serviceresult.setReturnCode(0);
 			serviceresult.setReturnMessage("成功");
@@ -144,15 +149,22 @@ public class AutomaticController {
 		return serviceresult.toJson();
 	}
 	@RequestMapping(value = "srv/dm/dmGetAutomaticPageUrl.srv", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
-	public String dmGetAutomaticPageUrl(@RequestParam("bizId") int bizId) {
+	public String dmGetAutomaticPageUrl(@RequestParam("sourceId") int sourceId,@RequestParam("sourceModular") String sourceModular,@RequestParam("pageName") String pageName) {
+		Map<String, String> map=new HashMap<String, String>(); 
+		RecordsetResult recordsetResult = new RecordsetResult();
+		try {
+		map= dmBizAutomatic.dmGetAutomaticPageUrl(sourceId,sourceModular,pageName);
 		
-		ServiceResult serviceresult = new ServiceResult();
-		String json= dmBizAutomatic.dmGetAutomaticPageUrl(bizId);
-		
-			serviceresult.setReturnCode(0);
-			serviceresult.setReturnMessage(json);
-		
-		return serviceresult.toJson();
+		recordsetResult.setReturnCode(0);
+		recordsetResult.setReturnMessage("成功");
+		recordsetResult.getRows().add(map);
+		} catch (Exception e) {
+			e.printStackTrace();
+			recordsetResult.setReturnCode(1);
+			recordsetResult.setReturnMessage("失败");
+			recordsetResult.getRows().add(map);
+		}
+		return recordsetResult.toJson();
 	}
 	
 	
@@ -176,10 +188,14 @@ public class AutomaticController {
 	}
 	
 	@RequestMapping(value = "srv/dm/dmCreateAutomaticConfig.srv", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
-	public String dmCreateAutomaticConfig(@RequestParam("mapColumn") String mapColumn) {
-		
+	public String dmCreateAutomaticConfig(HttpServletRequest request,@RequestParam("mapColumn") String mapColumn) {
+		User user = (User)request.getSession(false).getAttribute("user");
+		String userid=user.getId();
 		ServiceResult serviceresult = new ServiceResult();
 		JsonArray jsonArray= new JsonParser().parse(mapColumn).getAsJsonArray();
+		String idPrefix = "PG";
+		//获取工作表ID
+		String pageId = idFactory.newId(idPrefix);
 		for(int i =0;i<jsonArray.size();i++)
 		{
 			JsonObject jsonObject=jsonArray.get(i).getAsJsonObject();
@@ -188,7 +204,11 @@ public class AutomaticController {
 			String pageName=jsonObject.get("pageName").getAsString();
 			String panleName=jsonObject.get("panleName").getAsString();
 			JsonArray jsonArray2=new JsonParser().parse(jsonObject.get("column").getAsString()).getAsJsonArray();
-			if(dmBizAutomatic.dmCreateAutomaticConfig(sourceId,sourceModular,pageName,panleName,jsonArray2))
+			String state=jsonObject.get("state").getAsString();
+			String displayType=jsonObject.get("displayType").getAsString();
+			int isDelete=jsonObject.get("isDelete").getAsInt();
+			
+			if(dmBizAutomatic.dmCreateAutomaticConfig(sourceId,sourceModular,pageName,panleName,jsonArray2,state,displayType,userid,isDelete,pageId))
 			{
 				serviceresult.setReturnCode(0);
 				serviceresult.setReturnMessage("成功");
