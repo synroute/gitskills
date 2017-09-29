0aa9de31ba5dccd93d98bf113d214145a452fab5
synliuhao <synliuhao@synliuhao-PC>
2017-09-29 11:28:16 +0800

    dm修改

---
+++ src/hiapp/modules/dmsetting/DMBizAutomaticConfig.java
@@ -0,0 +1,50 @@
+package hiapp.modules.dmsetting;
+/** 
+ * @author liuhao
+ * @version 创建时间：2017年9月28日 下午20:00:09 
+ * 类说明  自动生成页面配置信息
+ */
+public class DMBizAutomaticConfig {
+	private String id;
+	private String sourceId;
+	private String sourceModulear;
+	private String pageName;
+	private String panleName;
+	private String config;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSourceId() {
+		return sourceId;
+	}
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
+	public String getPageName() {
+		return pageName;
+	}
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+	public String getSourceModulear() {
+		return sourceModulear;
+	}
+	public void setSourceModulear(String sourceModulear) {
+		this.sourceModulear = sourceModulear;
+	}
+	public String getPanleName() {
+		return panleName;
+	}
+	public void setPanleName(String panleName) {
+		this.panleName = panleName;
+	}
+	public String getConfig() {
+		return config;
+	}
+	public void setConfig(String config) {
+		this.config = config;
+	}
+}
