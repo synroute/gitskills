fb5eb496c8faf9c362ef1bf584c9d032baf3f2d3
majianbo <majianbohz@gmail.com>
2017-10-18 19:47:33 +0800

    update

--- src/hiapp/modules/dm/multinumbermode/bo/PhoneTypeDialSequence.java
+++ src/hiapp/modules/dm/multinumbermode/bo/PhoneTypeDialSequence.java
@@ -1,12 +1,19 @@
 package hiapp.modules.dm.multinumbermode.bo;
 
+import hiapp.modules.dmsetting.DMBizPhoneType;
+import hiapp.modules.dmsetting.data.DmBizPhoneTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class PhoneTypeDialSequence {
+
+    @Autowired
+    DmBizPhoneTypeRepository dmBizPhoneTypeRepository;
 
     // BizId <==> {拨打顺序 <==> 号码类型}
     Map<Integer, Map<Integer, Integer>> mapPhoneTypeDialSequence;
@@ -63,12 +70,17 @@ public class PhoneTypeDialSequence {
     }
 
     private Map<Integer, Integer> getBizPhoneTypeDialSequence(int bizId) {
-        Map<Integer, Integer> mapOneBizPhoneTypeDialSeq = getBizPhoneTypeDialSequence(bizId);
+        Map<Integer, Integer> mapOneBizPhoneTypeDialSeq = mapPhoneTypeDialSequence.get(bizId);
         if (null != mapOneBizPhoneTypeDialSeq)
             return  mapOneBizPhoneTypeDialSeq;
 
-        // TODO
-        //mapOneBizPhoneTypeDialSeq = dao.getBizPhoneTypeDialSequence(bizId);
+        mapOneBizPhoneTypeDialSeq = new HashMap<Integer, Integer>();
+
+        List<DMBizPhoneType> oneBizPhoneTypeList = dmBizPhoneTypeRepository.dmGetAllBizPhoneType(String.valueOf(bizId));
+        for (DMBizPhoneType dmBizPhoneType : oneBizPhoneTypeList) {
+            mapOneBizPhoneTypeDialSeq.put(dmBizPhoneType.getDialOrder(), dmBizPhoneType.getDialType());
+        }
+
         mapPhoneTypeDialSequence.put(bizId, mapOneBizPhoneTypeDialSeq);
         return mapOneBizPhoneTypeDialSeq;
     }
