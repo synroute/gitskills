950d2db69d25db7416041851a01f92f7e5b73d75
majianbo <majianbohz@gmail.com>
2017-11-14 15:11:24 +0800

    update

--- src/hiapp/modules/dm/Constants.java
+++ src/hiapp/modules/dm/Constants.java
@@ -2,7 +2,7 @@ package hiapp.modules.dm;
 
 public class Constants {
 
-    public static Long timeSlotSpan = 1000*60L; // 60 seconds
+    public static Long timeSlotSpan = 1000*30L; // 60 seconds
     public static Long timeoutThreshold = 2 * timeSlotSpan; //
 
 
