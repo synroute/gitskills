8d182269e8db9c8943871279854c28a16f772a93
李远 <@LAPTOP-F2284IS6>
2018-05-17 14:32:39 +0800

    123

--- src/hiapp/modules/exam/data/ExamDao.java
+++ src/hiapp/modules/exam/data/ExamDao.java
@@ -1,5 +1,7 @@
 package hiapp.modules.exam.data;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,9 +11,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.apache.logging.log4j.Logger;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
+import hiapp.modules.exam.utils.FtpUtil;
 import hiapp.modules.exam.utils.GsonUtil;
 import hiapp.utils.DbUtil;
 import hiapp.utils.database.BaseRepository;
@@ -164,19 +171,55 @@ public class ExamDao extends BaseRepository{
 		return  resultMap;
 	}
 	
-	
-	public  void  deleteQuestions(String questionIds) {
+	/**
+	 * 删除问题
+	 * @param questionIds
+	 * @return
+	 */
+	public  Map<String,Object>  deleteQuestions(String questionIds) {
 		Connection conn=null;
 		PreparedStatement pst=null;
 		String[] arr=questionIds.split(",");
+		Map<String,Object> resultMap=new HashMap<>();
 		try {
 			conn=this.getDbConnection();
-			String deleteSql="delete from EM_INF_EMQUESTIONBASE";
+			conn.setAutoCommit(false);
+			String deleteQuestionSql="delete from EM_INF_EMQUESTIONBASE where QUESTIONID=in(";
+			String deleteAnswerSql="delete from EM_INF_ANSWER where QUESTIONID in(";
+			for (int i = 0; i < arr.length; i++) {
+				String questionId=arr[i];
+				if(questionId==null&&"".equals(questionId)) {
+					continue;
+				}
+				deleteQuestionSql+="'"+questionId+"',";
+				deleteAnswerSql+="'"+questionId+"',";
+			}
+			deleteQuestionSql=deleteQuestionSql.substring(0,deleteQuestionSql.length()-1)+")";
+			deleteAnswerSql=deleteAnswerSql.substring(0,deleteAnswerSql.length()-1)+")";
+			pst=conn.prepareStatement(deleteQuestionSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			pst=conn.prepareStatement(deleteAnswerSql);
+			pst.executeUpdate();
+			conn.commit();
+			resultMap.put("dealSts","01");
+			resultMap.put("dealDesc","删除成功");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.info(e+"=========================");
+			resultMap.put("dealSts","02");
+			resultMap.put("dealDesc","删除成功");
 		}
+		
+		return resultMap;
 	}
+	/**
+	 * EXCEL导入题库
+	 * @param list
+	 * @param userId
+	 * @return
+	 */
 	public Map<String,Object> excelImportQuestion(List<Map<Integer,Object>> list,String userId) {
 		Connection conn=null;
 		PreparedStatement pst=null;
@@ -266,6 +309,7 @@ public class ExamDao extends BaseRepository{
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.info(e+"=========================");
 		}finally {
 			DbUtil.DbCloseQuery(rs, pst);
 			DbUtil.DbCloseConnection(conn);
@@ -336,6 +380,7 @@ public class ExamDao extends BaseRepository{
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.info(e+"=========================");
 		}finally {
 			DbUtil.DbCloseQuery(rs,pst);
 			DbUtil.DbCloseConnection(conn);
@@ -343,5 +388,240 @@ public class ExamDao extends BaseRepository{
 		return resultMap;
 	}
 	
+	/**
+	 * 添加考试信息
+	 * @param examName
+	 * @param startTime
+	 * @param endTime
+	 * @param isUsed
+	 * @param passLine
+	 * @param minLine
+	 * @param excellentLine
+	 * @param examType
+	 * @param userId
+	 * @return
+	 */
+	public Map<String,Object> insertExam(String examName,String startTime,String endTime,Integer isUsed,Integer passLine,Integer minLine,Integer excellentLine,Integer examType,String userId) {
+		Connection conn=null;
+		PreparedStatement pst=null;
+		String examId=idFactory.newId("EX_AM");
+		Map<String,Object> resultMap=new HashMap<>();
+		try {
+			conn=this.getDbConnection();
+			String sql="insert into EM_INF_EXAMINATION (EXAMINATIONID,EXAMINATIONNAME,STARTTIME,ENDTIME,ISUSED,CREATER,CREATTIME,PASSLINE,MEDIUMLINE,EXCELLENTLINE,EXAMTYPE) values(?,?,to_date(?,'yyyy-mm-dd hh24:mi:ss'),to_date(?,'yyyy-mm-dd hh24:mi:ss'),?,?,sysdate,?,?,?,?)";
+			pst=conn.prepareStatement(sql);
+			pst.setString(1, examId);
+			pst.setString(2, examName);
+			pst.setString(3, startTime);
+			pst.setString(4, endTime);
+			pst.setInt(5, isUsed);
+			pst.setString(6, userId);
+			pst.setInt(7, passLine);
+			pst.setInt(8, minLine);
+			pst.setInt(9, excellentLine);
+			pst.setInt(10, examType);
+			pst.executeUpdate();
+			resultMap.put("dealSts","01");
+			resultMap.put("dealDesc","添加成功");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			logger.info(e+"=========================");
+			resultMap.put("dealSts","02");
+			resultMap.put("dealDesc","添加失败");
+		}finally {
+			DbUtil.DbCloseExecute(pst);
+			DbUtil.DbCloseConnection(conn);
+		}
+		return resultMap;
+		
+	}
+	/**
+	 * 修改考试
+	 * @param examId
+	 * @param examName
+	 * @param startTime
+	 * @param endTime
+	 * @param isUsed
+	 * @param passLine
+	 * @param minLine
+	 * @param excellentLine
+	 * @param examType
+	 * @param userId
+	 * @return
+	 */
+	public Map<String,Object> updateExam(String examId,String examName,String startTime,String endTime,Integer isUsed,Integer passLine,Integer minLine,Integer excellentLine,Integer examType,String userId) {
+		Connection conn=null;
+		PreparedStatement pst=null;
+		Map<String,Object> resultMap=new HashMap<>();
+		try {
+			conn=this.getDbConnection();
+			String updateSql="update EM_INF_EXAMINATION set EXAMINATIONNAME=?,STARTTIME=to_date(?,'yyyy-mm-dd hh24:mi:ss'),endTime=to_date(?,'yyyy-mm-dd hh24:mi:ss'),ISUSED=?,CREATER=?,CREATTIME=sysdate,PASSLINE=?,"+
+							 "MEDIUMLINE=?,EXCELLENTLINE=?,EXAMTYPE=? where EXAMINATIONID=?";
+			pst=conn.prepareStatement(updateSql);
+			pst.setString(1, examName);
+			pst.setString(2, startTime);
+			pst.setString(3, endTime);
+			pst.setInt(4, isUsed);
+			pst.setString(5, userId);
+			pst.setInt(6, passLine);
+			pst.setInt(7, minLine);
+			pst.setInt(8, excellentLine);
+			pst.setInt(9, examType);
+			pst.setString(10, examId);
+			pst.executeUpdate();
+			resultMap.put("dealSts","01");
+			resultMap.put("dealDesc","修改成功");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			logger.info(e+"=========================");
+			resultMap.put("dealSts","02");
+			resultMap.put("dealDesc","修改失败");
+		}finally {
+			DbUtil.DbCloseExecute(pst);
+			DbUtil.DbCloseConnection(conn);
+		}
+		
+		return resultMap;
+	}
 
+	/**
+	 * 给考试选择试题
+	 * @param examId
+	 * @param questions
+	 * @param userId
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String,Object> insertQuestionForExam(String examId,String questions,String userId) {
+		Connection conn=null;
+		PreparedStatement pst=null;
+		List<Map<String,Object>> list=GsonUtil.getGson().fromJson(questions,List.class);
+		List<String> examationIdList=idFactory.newIds("EX_KS", list.size());
+		Map<String,Object> resultMap=new HashMap<>();
+		try {
+			conn=this.getDbConnection();
+			conn.setAutoCommit(false);
+			String questionIds="";
+			String useScores="";
+			for (int i = 0; i < list.size(); i++) {
+				Map<String,Object> map=list.get(i);
+				String questionId=(String) map.get("questionId");
+				String useScore=(String) map.get("useScore");
+				questionIds+=questionId+",";
+				useScores+=useScore+",";
+			}
+			questionIds=questionIds.substring(0,questionIds.length()-1).trim();
+			useScores=useScores.substring(0,useScores.length()-1).trim();
+			String examationIds=StringUtils.join(examationIdList.toArray(), ",");
+			String deleteAnwserSql="delete from EM_INF_EMANSWER a where exists (select b.EMQUESTIONID from EM_INF_EMQUESTION b where a.QUESTIONID=b.EMQUESTIONID and b.EXAMINATIONID='"+examId+"')";
+			pst=conn.prepareStatement(deleteAnwserSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			String deleteEmSql="delete from EM_INF_EMQUESTIONBASE a where exists (select b.EMQUESTIONID from EM_INF_EMQUESTION b where a.QUESTIONID=b.EMQUESTIONID and b.EXAMINATIONID='"+examId+"')";
+			pst=conn.prepareStatement(deleteEmSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			String deleteEmQuestionSql="delete from EM_INF_EMQUESTION where EXAMINATIONID='"+examId+"'";
+			pst=conn.prepareStatement(deleteEmQuestionSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			String insertEmQuestionSql="insert into EM_INF_EMQUESTION(EMQUESTIONID,EXAMINATIONID,QUESTIONID,DEFAULSCORE,USESCORE) select b.examationId,'"+examId+"',b.questionId,a.DEFAULSCORE,b.useScore from EM_INF_QUESTIONBASE a right join (select "+
+									   "REGEXP_SUBSTR ('"+examationIds+"', '[^,]+', 1,rownum) examationId,REGEXP_SUBSTR ('"+questionIds+"', '[^,]+', 1,rownum) questionId,REGEXP_SUBSTR ('"+useScores+"', '[^,]+', 1,rownum) useScore from dual "+
+									   "CONNECT BY ROWNUM <=LENGTH ('"+examationIds+"') - LENGTH (REPLACE ('"+examationIds+"', ',', ''))+1) b on a.QUESTIONID=b.questionId";
+			pst=conn.prepareStatement(insertEmQuestionSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			String insertEmSql="insert into EM_INF_EMQUESTIONBASE(QUESTIONID,QUESTIONBODY,QUESTIONTYPENAME,QUESTIONBODYTYPENAME,QUESTIONLEVENAME,DEFAULSCORE,INPUTTIME,INPUTER,ISUSED,ISUPDATE,FTPATH) "+
+							 "select a.examationId,b.QUESTIONDES,b.QUESTIONCLASS,b.QUESTIONTYPE,b.QUESTIONLEVE,a.DEFAULSCORE,sysdate,'"+userId+"',b.ISUSED,1,b.FTPATH from EM_INF_EMQUESTION a left join EM_INF_QUESTIONBASE b on a.QUESTIONID=b.QUESTIONID where a.EXAMINATIONID='"+examId+"'";
+			
+			pst=conn.prepareStatement(insertEmSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			String insertAnwserSql="insert into EM_INF_EMANSWER(ANSWERID,QUESTIONID,ANSWERSN,ANSWERBODY,ISRIGHT) select S_EM_INF_ANSWER.NEXTVAL,b.EMQUESTIONID,a.ANSWERSN,a.ANSWERBODY,a.ISRIGHT from EM_INF_ANSWER a right join EM_INF_EMQUESTION b on a.QUESTIONID=b.QUESTIONID where b.EXAMINATIONID='"+examId+"'";
+			pst=conn.prepareStatement(insertAnwserSql);
+			pst.executeUpdate();
+			DbUtil.DbCloseExecute(pst);
+			conn.commit();
+			resultMap.put("dealSts","01");
+			resultMap.put("dealDesc","保存成功");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			logger.info(e+"=========================");
+			resultMap.put("dealSts","02");
+			resultMap.put("dealDesc","保存失败");
+		}finally {
+			DbUtil.DbCloseExecute(pst);
+			DbUtil.DbCloseConnection(conn);
+		}
+		
+		return resultMap;
+	} 
+	/**
+	 * 查询试题
+	 * @param examId
+	 * @param examPath
+	 * @return
+	 */
+	public Map<String,Object> selectExamInfo(String examId,String examPath) {
+		Connection conn=null;
+		PreparedStatement pst=null;
+		ResultSet rs=null;
+		List<Map<String,Object>> questionList=new ArrayList<>();
+		List<Map<String,Object>> anwserList=new ArrayList<>();
+		Map<String,Object> resultMap=new HashMap<>();
+		try {
+			conn=this.getDbConnection();
+		
+			String getQuestionSql="select a.QUESTIONID,a.QUESTIONBODY,a.QUESTIONTYPENAME,a.QUESTIONBODYTYPENAME,a.QUESTIONSTYLENAME,a.QUESTIONLEVENAME,a.ftpPath,b.USESCORE from EM_INF_EMQUESTIONBASE a right join EM_INF_EMQUESTION b on a.QUESTIONID=b.EMQUESTIONID where b.EXAMINATIONID='"+examId+"'";
+			pst=conn.prepareStatement(getQuestionSql);
+			rs=pst.executeQuery();
+			while(rs.next()) {
+				Map<String,Object> map=new HashMap<>();
+				map.put("questionId", rs.getObject(1));
+				map.put("questionBody", rs.getObject(2));
+				map.put("questionType", rs.getObject(3));
+				map.put("questionBodyType", rs.getObject(4));
+				map.put("quesType", rs.getObject(5));
+				map.put("questionLevel", rs.getObject(6));
+				map.put("ftpPath", rs.getObject(7));
+				map.put("score", rs.getObject(8));
+				questionList.add(map);
+			}
+			DbUtil.DbCloseQuery(rs, pst);
+			for (int i = 0; i < questionList.size(); i++) {
+				Map<String,Object> map=questionList.get(i);
+				String questionId=(String) map.get("questionId");
+				String ftpPath=(String) map.get("ftpPath");
+				if(ftpPath!=null&&!"".equals(ftpPath)) {
+					String newName = FtpUtil.getExamFile(ftpPath, examPath, questionId);
+					map.put("ftpPath","/exam/"+newName);
+				}
+			}
+			String getAnwserSql="select a.ANSWERID,a.QUESTIONID,a.ANSWERSN,a.ANSWERBODY,a.ISRIGHT from EM_INF_ANSWER a right join EM_INF_EMQUESTION b where a.QUESTIONID=b.EMQUESTIONID where EXAMINATIONID='"+examId+"' order by ANSWERSN asc";
+			pst=conn.prepareStatement(getAnwserSql);
+			rs=pst.executeQuery();
+			while(rs.next()) {
+				Map<String,Object> map=new HashMap<>();
+				map.put("anwserId", rs.getObject(1));
+				map.put("questionId", rs.getObject(2));
+				map.put("anwsern", rs.getObject(3));
+				map.put("anwserBody", rs.getObject(4));
+				map.put("isRight", rs.getObject(5));
+				anwserList.add(map);
+			}
+			resultMap.put("questionList", questionList);
+			resultMap.put("anwserList", anwserList);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultMap;
+	}
+	
+	public void saveEaxmScroe() {
+		
+	}
 }
