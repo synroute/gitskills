27b03215878b5ee31ad58def16caa3f7cb479b61
synliuhao <lenovo@192.168.23.3>
2018-06-17 16:16:57 +0800

    1

---
+++ src/hiapp/modules/dmsetting/DMBizQusResult.java
@@ -0,0 +1,95 @@
+package hiapp.modules.dmsetting;
+
+public class DMBizQusResult {
+	 private String SOURCEID;
+	 private String IID; 
+	 private String CID; 
+	 private String MODIFYID; 
+	 private String MODIFYUSERID;
+	 private String MODIFYTIME; 
+	 private String MODIFYLAST; 
+	 private String QUS_ID; 
+	 private String QUS_INDEX;
+	 private String QUS_TYPE; 
+	 private String ORG_CONTENT; 
+	 private String ORG_RES;
+	 private String RES_INDEX;
+	public String getSOURCEID() {
+		return SOURCEID;
+	}
+	public void setSOURCEID(String sOURCEID) {
+		SOURCEID = sOURCEID;
+	}
+	public String getIID() {
+		return IID;
+	}
+	public void setIID(String iID) {
+		IID = iID;
+	}
+	public String getCID() {
+		return CID;
+	}
+	public void setCID(String cID) {
+		CID = cID;
+	}
+	public String getMODIFYUSERID() {
+		return MODIFYUSERID;
+	}
+	public void setMODIFYUSERID(String mODIFYUSERID) {
+		MODIFYUSERID = mODIFYUSERID;
+	}
+	public String getMODIFYID() {
+		return MODIFYID;
+	}
+	public void setMODIFYID(String mODIFYID) {
+		MODIFYID = mODIFYID;
+	}
+	public String getMODIFYTIME() {
+		return MODIFYTIME;
+	}
+	public void setMODIFYTIME(String mODIFYTIME) {
+		MODIFYTIME = mODIFYTIME;
+	}
+	public String getQUS_ID() {
+		return QUS_ID;
+	}
+	public void setQUS_ID(String qUS_ID) {
+		QUS_ID = qUS_ID;
+	}
+	public String getMODIFYLAST() {
+		return MODIFYLAST;
+	}
+	public void setMODIFYLAST(String mODIFYLAST) {
+		MODIFYLAST = mODIFYLAST;
+	}
+	public String getQUS_INDEX() {
+		return QUS_INDEX;
+	}
+	public void setQUS_INDEX(String qUS_INDEX) {
+		QUS_INDEX = qUS_INDEX;
+	}
+	public String getQUS_TYPE() {
+		return QUS_TYPE;
+	}
+	public void setQUS_TYPE(String qUS_TYPE) {
+		QUS_TYPE = qUS_TYPE;
+	}
+	public String getORG_CONTENT() {
+		return ORG_CONTENT;
+	}
+	public void setORG_CONTENT(String oRG_CONTENT) {
+		ORG_CONTENT = oRG_CONTENT;
+	}
+	public String getORG_RES() {
+		return ORG_RES;
+	}
+	public void setORG_RES(String oRG_RES) {
+		ORG_RES = oRG_RES;
+	}
+	public String getRES_INDEX() {
+		return RES_INDEX;
+	}
+	public void setRES_INDEX(String rES_INDEX) {
+		RES_INDEX = rES_INDEX;
+	} 
+}
